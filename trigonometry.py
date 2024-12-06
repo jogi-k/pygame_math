0579ced1c46c526877e5fe48f5a8007a8d90854f
Jörg ( Jogi ) Künstner <jogi@kuenstner.de>
2024-12-06 08:43:25 +0100

    Create trigonometry.py

---
+++ trigonometry.py
@@ -0,0 +1,56 @@
+import math
+WIDTH = 1000
+HEIGHT = 1000
+
+mitte_x = WIDTH / 2
+mitte_y = HEIGHT / 2
+
+radius = 4 * HEIGHT / 10
+
+winkel = 30
+
+
+def draw():
+    screen.clear()
+    screen.draw.circle(( mitte_x , mitte_y ), radius , 'white')
+    
+    sinus = math.sin( math.radians( winkel ) ) 
+    cosinus = math.cos( math.radians( winkel ) )  
+    sinu_r = sinus * radius
+    cosi_r = cosinus * radius
+
+    screen.draw.line(( mitte_x , mitte_y ),( mitte_x + cosi_r, mitte_y - sinu_r ), 'white')
+    screen.draw.line(( mitte_x , mitte_y ),( mitte_x + cosi_r, mitte_y ), 'red')
+    screen.draw.line(( mitte_x + cosi_r , mitte_y ),( mitte_x + cosi_r, mitte_y - sinu_r ), 'green')
+
+    mytext = "Winkel = " + str( winkel) + "\nSinus = " + str(sinus) + "\nCosinus = " + str(cosinus)      
+
+    screen.draw.text( mytext, ( 10, 10) )
+
+
+def on_key_down( key ):
+    global winkel
+
+    if key == key.UP:
+        winkel = winkel + 10
+    if key == key.DOWN:
+        winkel = winkel - 10
+    if key == key.LEFT:
+        winkel = winkel - 1
+    if key == key.RIGHT:
+        winkel = winkel + 1
+    if key == key.W :
+        winkel = 90
+    if key == key.A :
+        winkel = 180
+    if key == key.S :
+        winkel = 270
+    if key == key.D :
+        winkel = 0
+
+    
+    if winkel > 360 :
+        winkel = winkel - 360
+    if winkel < 0 :
+        winkel = 360 + winkel   
+
